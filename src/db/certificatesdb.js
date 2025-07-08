ed3699161b095adac0e00fba5495bf4cd141e8d4
Rahul <baghel931515@gmail.com>
2025-07-08 13:55:12 +0100

    added new certificates

--- src/db/certificatesdb.js
+++ src/db/certificatesdb.js
@@ -2,8 +2,34 @@ import JavaImg from '../images/Java.png';
 import Japanese from '../images/Japanese .png';
 import KotlinandJava from '../images/ktlin and Java.png';
 import PythonBootcamp from '../images/Python Bootcamp.png';
-
+import Sjogcertificate from '../images/Sjog_certificate.png';
 export const CertifiatesList = [
+
+  {
+    //heading: " INDIAN STATES",
+    img: PythonBootcamp,
+    title: " 100 Days Python Bootcamp",
+    description:
+      "Completed a comprehensive 100-day journey into Python programming. Learned core concepts including OOP, web development with Flask, automation, data analysis, and APIs. Built real-world projects such as web scrapers, games, and fullstack apps using Python.",
+  
+  },
+
+  {
+    
+    img: Sjogcertificate,
+    title: "Certificate of Contribution in SJOG",
+    description:
+    "As part of the TU Dublin CoDesign Team Projects 2023–2024, I developed a Talking Clock Android application for Saint John of God (SJOG) services. Designed using Java and Android Studio, the app assists individuals with visual or cognitive impairments by providing voice-based time notifications. The project was built in collaboration with real-world users and emphasized accessibility, usability, and inclusive design under faculty guidance.",
+  },
+
+  {
+    //heading: "E.M.S",
+    img: KotlinandJava,
+    title: "Android Studio (Kotlin and Java)",
+    description:
+    "Learned to build Android apps from scratch using both Java and Kotlin. Covered Android Studio, UI design, activities, fragments, and integrating APIs. Built functional mobile apps and understood the lifecycle of Android development.",
+  },
+  
   {
     
     img: JavaImg,
@@ -11,6 +37,7 @@ export const CertifiatesList = [
     description:
     "Developed solid command over Java from scratch to advanced topics. Learned object-oriented programming, data structures, exception handling, and core Java APIs. Built projects and mastered Java logic building useful for both backend and Android development.",
   },
+
   {
     //heading: "AṞIVU",
     img: Japanese,
@@ -20,19 +47,8 @@ export const CertifiatesList = [
   },
 
   
-  {
-    //heading: " INDIAN STATES",
-    img: PythonBootcamp,
-    title: " 100 Days Python Bootcamp",
-    description:
-      "Completed a comprehensive 100-day journey into Python programming. Learned core concepts including OOP, web development with Flask, automation, data analysis, and APIs. Built real-world projects such as web scrapers, games, and fullstack apps using Python.",
   
-  },
-  {
-    //heading: "E.M.S",
-    img: KotlinandJava,
-    title: "Android Studio (Kotlin and Java)",
-    description:
-    "Learned to build Android apps from scratch using both Java and Kotlin. Covered Android Studio, UI design, activities, fragments, and integrating APIs. Built functional mobile apps and understood the lifecycle of Android development.",
-  },
+  
+
+
 ];
